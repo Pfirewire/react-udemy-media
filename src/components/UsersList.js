40530f9a7740ff901be60e7bb75900101f10eebb
Stephen Pflug <stephen@pflug.us>
2023-02-18 11:02:28 -0600

    Creating a Reusable Thunk Hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,33 +1,23 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
+import useThunk from "../hooks/useThunk";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 function UsersList() {
-    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-    const [loadingUsersError, setLoadingUsersError] = useState(null);
-    const [isCreatingUser, setIsCreatingUser] = useState(false);
-    const [creatingUserError, setCreatingUserError] = useState(null);
-    const dispatch = useDispatch();
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
     const { data } = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        setIsLoadingUsers(true);
-        dispatch(fetchUsers())
-            .unwrap()
-            .catch(error => setLoadingUsersError(error))
-            .finally(() => setIsLoadingUsers(false));
-    }, [dispatch]);
+        doFetchUsers();
+    }, [doFetchUsers]);
 
     const handleAddUser = () => {
-        setIsCreatingUser(true);
-        dispatch(addUser())
-            .unwrap()
-            .catch(error => setCreatingUserError(error))
-            .finally(() => setIsCreatingUser(false));
+        doCreateUser();
     };
 
     if(isLoadingUsers) {
