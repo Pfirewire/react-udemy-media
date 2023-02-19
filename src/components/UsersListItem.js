778d459609e9458e71b607c5395f2cef40212b88
Stephen Pflug <stephen@pflug.us>
2023-02-19 15:09:29 -0600

    Fixing a Delete Error

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -4,7 +4,7 @@ import { removeUser } from "../store";
 import { useThunk } from "../hooks/useThunk";
 
 function UsersListItem({ user }) {
-    const [doRemoveUser, isLoading, error] = useThunk(removeUser());
+    const [doRemoveUser, isLoading, error] = useThunk(removeUser);
 
     const handleClick = () => {
         doRemoveUser(user);
@@ -13,11 +13,13 @@ function UsersListItem({ user }) {
     return(
         <div key={user.id} className='mb-2 border rounded'>
             <div className='flex p-2 justify-between items-center cursor-pointer'>
-                <Button loading={isLoading} onClick={handleClick}>
-                    <GoTrashcan />
-                </Button>
-                {error && <div>Error deleting user.</div>}
-                {user.name}
+                <div className='flex flex-row items-center justify-between'>
+                    <Button className='mr-3' loading={isLoading} onClick={handleClick}>
+                        <GoTrashcan/>
+                    </Button>
+                    {error && <div>Error deleting user.</div>}
+                    {user.name}
+                </div>
             </div>
         </div>
     );
